1ab4beb4b409dd272510daa5ff58bdcd4708515a
Hidekazu Onishi <the-o@neotitans.net>
2017-01-18 23:28:24 +0900

    Update patches to build tensorflow on Odroid C2

--- build_tensorflow/build_tensorflow_py3.sh
+++ build_tensorflow/build_tensorflow_py3.sh
@@ -14,57 +14,42 @@ cd work3
 source bin/activate
 
 # Build protobuf
-# For grpc-java build
 git clone https://github.com/google/protobuf.git
 cd protobuf
-git checkout tags/v3.0.0-beta-3
+git checkout tags/v3.0.2
 ./autogen.sh
-LDFLAGS=-static ./configure --prefix=$(pwd)/../
-sed -i -e 's/LDFLAGS = -static/LDFLAGS = -all-static/' ./src/Makefile
+./configure --prefix=$(pwd)/../
 make -j4
 make install
-
-# For bazel build
-git checkout tags/v3.0.0-beta-2
-./autogen.sh
-LDFLAGS=-static ./configure --prefix=$(pwd)/../
-sed -i -e 's/LDFLAGS = -static/LDFLAGS = -all-static/' ./src/Makefile
-make -j4
 cd ..
 
 # Build grpc-java compiler
 git clone https://github.com/grpc/grpc-java.git
 cd grpc-java/
-git checkout tags/v0.15.0
-patch -p0 < ../../grpc-java.v0.15.0.patch
+git checkout tags/v1.0.3
+patch -p0 < ../../grpc-java.v1.0.3.patch
 CXXFLAGS="-I$(pwd)/../include" LDFLAGS="-L$(pwd)/../lib" ./gradlew java_pluginExecutable -Pprotoc=$(pwd)/../bin/protoc
+cp compiler/build/exe/java_plugin/protoc-gen-grpc-java ../bin/
 cd ..
 
 # Build bazel
 git clone https://github.com/bazelbuild/bazel.git
 cd bazel
-git checkout 47be2a40c601b5e4737f7a6825fad7e7f6ce0302
-
-cp ../protobuf/src/protoc third_party/protobuf/protoc-linux-aarch64.exe
-cp ../grpc-java/compiler/build/exe/java_plugin/protoc-gen-grpc-java third_party/grpc/protoc-gen-grpc-java-0.15.0-linux-aarch64.exe
-
-patch -p0 < ../../bazel.47be2a4.patch
-./compile.sh
-
+git checkout tags/0.4.3
+patch -p0 < ../../bazel.0.4.3.patch
+PROTOC=$(pwd)/../bin/protoc GRPC_JAVA_PLUGIN=$(pwd)/../bin/protoc-gen-grpc-java ./compile.sh
 cp output/bazel ../bin/
 cd ..
 
 # Build TensorFlow
 git clone --recurse-submodules https://github.com/tensorflow/tensorflow
 cd tensorflow
-git checkout tags/v0.10.0rc0 # 3cb39956e622b322e43547cf2b6e337020643f21
-patch -p0 < ../../tensorflow.v0.10.0rc0.patch
-
-PYTHON_BIN_PATH=$(pwd)/../bin/python TF_NEED_GCP=0 TF_NEED_CUDA=0 ./configure
+git checkout origin/r0.12
+PYTHON_BIN_PATH=$(pwd)/../bin/python PYTHON_LIB_PATH=$(pwd)/../lib/python3.5/site-packages TF_NEED_GCP=0 TF_NEED_CUDA=0 TF_NEED_HDFS=0 TF_NEED_OPENCL=0 ./configure
 bazel build -c opt --local_resources 1536,0.5,1.0 --verbose_failures //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
 cd ..
 
 # Install and run
-pip install /tmp/tensorflow_pkg/tensorflow-0.10.0rc0-py3-none-any.whl
+pip install /tmp/tensorflow_pkg/tensorflow-0.12.1-cp35-cp35m-linux_aarch64.whl
 time python lib/python3.5/site-packages/tensorflow/models/image/mnist/convolutional.py
